683d7cd864b24e922d534f93ce9560b723fd9868
productdevbook <hi@productdevbook.com>
2025-11-08 20:20:21 +0300

    fix: update module declaration from 'h3' to 'nitro/h3' in context definition

--- server/graphql/context.d.ts
+++ server/graphql/context.d.ts
@@ -1,7 +1,7 @@
 // Example context definition - please change it to your needs
 // import type { Database } from '../utils/useDb'
 
-declare module 'h3' {
+declare module 'nitro/h3' {
   interface H3EventContext {
     // Add your custom context properties here
     // useDatabase: () => Database
